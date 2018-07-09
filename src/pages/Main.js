ff457409fc12f1505f5c263deaa04378348cae1e
KATT <alexander@n1s.se>
2018-07-09 12:29:46 +0200

    Fix formatting

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,55 +1,50 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
-import AddPlant from './AddPlant'
+import AddPlant from './AddPlant';
 
 class Main extends Component {
-
   constructor() {
     super();
     this.state = {
-      addplantVisable: false
-    }
-
+      addplantVisable: false,
+    };
   }
 
   onAddFirstPlant() {
-      this.setState ({
-      addplantVisable: true
-    })
+    this.setState({
+      addplantVisable: true,
+    });
   }
 
   goBackTest(plantState) {
-    this.setState ({
-      addplantVisable: false
-    })
+    this.setState({
+      addplantVisable: false,
+    });
     //Plantstate is the info coming from the form in AddPlant
-    console.log(plantState)
+    console.log(plantState);
   }
 
   render() {
+    let htmlContent = '';
 
+    if (this.state.addplantVisable === true) {
+      htmlContent = (
+        <AddPlant
+          history={this.props.history}
+          goBackTest={e => this.goBackTest(e)}
+        />
+      );
+    } else {
+      htmlContent = (
+        <div>
+          <button onClick={e => this.onAddFirstPlant(e)}>Add a plant</button>
+        </div>
+      );
+    }
 
-
-    let htmlContent = "";
-
-        if(this.state.addplantVisable === true) {
-          htmlContent = <AddPlant history={this.props.history} goBackTest={(e) => this.goBackTest(e)}/>
-        } else {
-          htmlContent =
-          <div>
-            <button onClick={(e) => this.onAddFirstPlant(e)}>Add a plant</button>
-          </div>
-
-        }
-
-    return (
-      <div className="center">
-        {htmlContent}
-      </div>
-
-    );
+    return <div className="center">{htmlContent}</div>;
   }
 }
 
