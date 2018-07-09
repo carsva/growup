7e2375af2fc9a130ae81dcfdd6f3336ad442ea88
KATT <alexander@n1s.se>
2018-07-09 13:21:51 +0200

    Add plant `<Link`

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,46 +5,12 @@ import Footer from './../components/Footer';
 import AddPlant from './AddPlant';
 
 class Main extends Component {
-  constructor() {
-    super();
-    this.state = {
-      addplantVisable: false,
-    };
-  }
-
-  onAddFirstPlant() {
-    this.setState({
-      addplantVisable: true,
-    });
-  }
-
-  goBackTest(plantState) {
-    this.setState({
-      addplantVisable: false,
-    });
-    //Plantstate is the info coming from the form in AddPlant
-    console.log(plantState);
-  }
-
   render() {
-    let htmlContent = '';
-
-    if (this.state.addplantVisable === true) {
-      htmlContent = (
-        <AddPlant
-          history={this.props.history}
-          goBackTest={e => this.goBackTest(e)}
-        />
-      );
-    } else {
-      htmlContent = (
-        <div>
-          <button onClick={e => this.onAddFirstPlant(e)}>Add a plant</button>
-        </div>
-      );
-    }
-
-    return <div className="center">{htmlContent}</div>;
+    return (
+      <div>
+        <Link to="/addplant">Add plant</Link>
+      </div>
+    );
   }
 }
 
