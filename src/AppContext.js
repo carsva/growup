7e384aaeaeab3615102139e115133b178a415949
Carl Svahnstrom <carl@tocco.se>
2018-07-19 14:13:56 +0200

    Added documentation

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+//We create our context
 export const AppContext = React.createContext('plant');
 
+//We create a const of id
 let id = 0;
 
+/*We make a function to give every object it own Id by plussing 1
+to every new Id. We add the empty string just because we need to
+make it into a string. Why is not clear to me yet! :) 
+ */
 const getId = () => ++id + '';
 
+//We set up a default plant object
 const defaultPlant = {
   id: null,
   name: null,
@@ -19,18 +26,35 @@ function newPlant() {
     id: getId(),
   };
 }
-
+/* We create our global context state here below. 
+All other components will have access to this state*/
 export class AppProvider extends React.Component {
   state = {
     theme: 'light',
     superhero: 'Calle',
     plants: [
       {
+        /* Adds new plant into this object using
+        the spread operator so that we will have
+        a start object with values to start with
+        looking like below
+        {
+            "id": "1",
+            "name": "Something",
+            "amount": null,
+            "timesPerMonth": null
+        }*/
+        
         ...newPlant(),
         name: 'Something',
       },
     ],
   };
+
+  /* The function below uses the ternery operator to 
+  ask if the theme is set to light and if it is. It should
+  set it to dark. If not set to light, then it should be
+  set to light */
 
   toggleTheme = () => {
     this.setState(({ theme }) => ({
@@ -40,7 +64,7 @@ export class AppProvider extends React.Component {
 
   editPlant = (id, newValues) => {
     {
-      /*3.The name is passed into the (e)  */
+      
     }
 
     this.setState({
@@ -58,15 +82,6 @@ export class AppProvider extends React.Component {
   };
 
   newPlant = values => {
-    // const obj = {
-    //   foo: 'bar',
-    // }
-
-    // const obj2 = {
-    //   ...obj,
-    //   zoo: 'cow',
-    // }
-    // const obj3 = Object.assign({}, obj)
 
     this.setState({
       ...this.state,
@@ -80,6 +95,12 @@ export class AppProvider extends React.Component {
     });
   };
 
+  /**
+   Here we create the object that should be distributed to
+   all other components. The global state is put into a
+   const for export. All in the value const will be available
+   for the other components
+   */
   render() {
     const value = {
       ...this.state,
@@ -87,7 +108,12 @@ export class AppProvider extends React.Component {
       newPlant: this.newPlant,
       editPlant: this.editPlant,
     };
-
+    /** 
+     Now we pass the value const into the value property in order
+     to distribute it to all AppConsumers. Thats how you fetch
+     the data from the global context. All components that uses
+     the AppConsumer is children of the provider.
+    */
     return (
       <AppContext.Provider value={value}>
         {this.props.children}
--- src/pages/Plants.js
+++ src/pages/Plants.js
@@ -5,11 +5,15 @@ import { AppConsumer } from '../AppContext';
 class Plants extends Component {
   render() {
     return (
+      /* We start with making the AppConsumer the parent tag and under
+      it we insert the global state from AppContext.js*/
       <AppConsumer>
         {state => (
           <div>
             <h2>Plants</h2>
             <ul>
+              /** Below we map through all the plants set in the global state in
+              an unordered list showing the name of it. */
               {state.plants.map(({ id, name }) => (
                 <li key={id}>
                   Plant: {name} <Link to={`/plants/edit/${id}`}>Edit</Link>
